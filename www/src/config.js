a5ddb8a8fb17a441132463c3c18e53a269216e1e
尹锋 <yinfeng@mgtv.com>
2020-11-30 20:02:49 +0800

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "None",     // CognitoClientID
-  "api_base_url": "None",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "None",                   // CognitoDomainName
-  "redirect_url": "None"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "1akm2m48v294u0obotg7q3ul94",     // CognitoClientID
+  "api_base_url": "https://sx3h0yzz25.execute-api.us-west-2.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-lambda-refarch-webapp.auth.us-west-2.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d15wl1mg289jl6.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
